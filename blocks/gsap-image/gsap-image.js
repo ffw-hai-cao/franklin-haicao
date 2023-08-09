bf462c19ff8562e3cb2363cfd0705e591f5353dc
Hai Cao <hai.cao@pfizer.com>
2023-08-09 10:59:38 +0700

    Update gsap image effect

--- blocks/gsap-image/gsap-image.js
+++ blocks/gsap-image/gsap-image.js
@@ -1,40 +1,94 @@
-async function loadGsap() {
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-undef */
+import { loadScript } from '../../scripts/lib-franklin.js';
+
+function loadGsap() {
   // Initialize ScrollTrigger
   gsap.registerPlugin(ScrollTrigger);
+
   // Animation for section 3
-  gsap.to('.default-content-wrapper img', {
+  gsap.to('.image-gsap img', {
     scrollTrigger: {
-      trigger: '#section3',
-      start: 'top center',
-      end: 'bottom center',
+      trigger: '#section-3',
+      start: 'bottom bottom',
+      end: 'bottom bottom',
       scrub: true,
     },
-    width: '600px', // Bigger by 10%
-    bottom: '0',
-    left: '0',
+    width: '256px',
     opacity: 1,
+  });
+
+  // Animation for section 3
+  gsap.fromTo('.image-gsap img', {
+    width: '256px',
+  }, {
+    scrollTrigger: {
+      trigger: '#section-3',
+      start: 'bottom bottom',
+      end: 'bottom top',
+      scrub: true,
+    },
+    width: '400px',
+    bottom: 0,
+    left: 0,
     ease: 'power1.out',
   });
   // Animation for section 4
-  gsap.to('.default-content-wrapper img', {
+  gsap.fromTo('.image-gsap img', {
+    width: '400px',
+  }, {
     scrollTrigger: {
-      trigger: '#section4',
-      start: 'top center',
-      end: 'bottom center',
+      trigger: '#section-4',
+      start: 'bottom center',
+      end: 'bottom top',
       scrub: true,
     },
-    width: '1200px', // Bigger by 100%
+    width: '600px',
   });
-  gsap.to('.default-content-wrapper img', {
+  // Animation for section 5 and 6
+  gsap.fromTo('.image-gsap img', {
+    width: '600px',
+  }, {
     scrollTrigger: {
-      trigger: '#section5',
-      start: 'top center',
-      end: 'bottom center',
+      trigger: '#section-6',
+      start: 'top bottom',
+      end: 'bottom bottom',
       scrub: true,
     },
-    width: '400px', // Smaller by 50%
-    opacity: 0,
+    width: '256px',
+    bottom: 0,
+    right: 0,
+    left: 'auto',
   });
 }
 
-loadGsap();
+export default async function decorate(block) {
+  const cols = [...block.firstElementChild.children];
+  block.classList.add('columns', `columns-${cols.length}-cols`);
+
+  // setup image columns
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((col) => {
+      const pic = col.querySelector('picture');
+      if (pic) {
+        const picWrapper = pic.closest('div');
+        if (picWrapper && picWrapper.children.length === 2) {
+          // picture is only content in column
+          picWrapper.classList.add('columns-img-col', 'section-gsap-image');
+          picWrapper.lastElementChild.classList.add('image-gsap');
+        }
+      }
+    });
+  });
+
+  const sections = document.querySelectorAll('.section');
+  sections.forEach((section, index) => {
+    section.setAttribute('id', `section-${index + 1}`);
+  });
+
+  await loadScript(`${window.hlx.codeBasePath}/scripts/gsap.min.js`);
+  await loadScript(`${window.hlx.codeBasePath}/scripts/ScrollTrigger.min.js`);
+  setTimeout(() => {
+    loadGsap();
+  }, 500);
+}
